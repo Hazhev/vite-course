5170ba0b63fa813127c7cb6427251e082cb2b110
Мухамед <maclarengtr@mail.ru>
2025-02-06 12:31:38 +0300

    2 модуль, многостраничный сайт

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from "vite";
+// оптимизация работы с графикой
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer"; // npm install vite-plugin-image-optimizer sharp svgo --save-dev
+
+// векторный спрайт
+import createSVGSpritePlugin from "vite-plugin-svg-spriter"; // npm i vite-plugin-svg-spriter
+
+import { resolve } from "path";
+
+const FRONT_PATH = "src";
+
+export default defineConfig({
+  root: "src",
+  build: {
+    minify: true,
+    cssMinify: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        index: resolve(`${FRONT_PATH}/index.html`),
+        about: resolve(__dirname, `${FRONT_PATH}/html/about/index.html`),
+        catalog: resolve(__dirname, `${FRONT_PATH}/html/catalog/index.html`),
+      },
+    },
+  },
+  plugins: [
+    ViteImageOptimizer({
+      jpg: {
+        quality: 75,
+      },
+      png: {
+        quality: 75,
+      },
+    }),
+    createSVGSpritePlugin({
+      svgFolder: resolve(__dirname, "./public"),
+    }),
+  ],
+});
