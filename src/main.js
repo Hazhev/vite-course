c66c3afd1ae8d7be46ba06fc4e05ca993916aae3
Мухамед <maclarengtr@mail.ru>
2025-02-06 15:19:44 +0300

    3 модуль, окружение, прод, дев

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
-import "./style.css";
-
-console.log(import.meta.env);
+const message = `Проект собран в окружении ${import.meta.env.MODE}, API_URL - ${
+  import.meta.env.VITE_API_URL
+}`;
+console.log(message);
